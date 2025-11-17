fad58b7f644cfac6ba90f556aca2a14a3d2f2d79
yoda_004 <shivamdhariwal.13@gmail.com>
2025-11-18 01:21:31 +0530

    Revert "finalization of the project"
    This reverts commit bbb8e7c8e7c5f153b00ce07108a2dcf80d8920e0.

--- StudentManagementApp/MainWindow.xaml.cs
+++ StudentManagementApp/MainWindow.xaml.cs
@@ -11,19 +11,6 @@ namespace StudentManagementApp
         {
             InitializeComponent();
 
-            try
-            {
-                DataContext = new ViewModels.MainViewModel();
-            }
-            // ...and 'catch' any error that happens.
-            catch (Exception ex)
-            {
-                // Show the error in a popup so we can read it!
-                MessageBox.Show($"Fatal error on startup: {ex.Message}\n\n{ex.StackTrace}", "Startup Error", MessageBoxButton.OK, MessageBoxImage.Error);
-
-                // Close the app
-                Application.Current.Shutdown();
-            }
             // This one line connects your View (XAML) to your ViewModel (C# Logic)
             DataContext = new ViewModels.MainViewModel();
         }
